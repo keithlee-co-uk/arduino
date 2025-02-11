89ca343beeeb85b3b89cb8367b2bf10dc64d4c5f
Keith Lee <kl250241@ncr.com>
2025-02-11 17:31:16 +0000

    Back to basics

--- sketch_receiver/sketch_receiver.ino
+++ sketch_receiver/sketch_receiver.ino
@@ -1,26 +1,24 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
 
-RH_ASK driver(1000); // rxPin 11
+RH_ASK driver;
 
 void setup()
 {
-  Serial.begin(9600);	// Debugging only
-  delay(2000);
-  if (!driver.init())
-    Serial.println("init failed");
-  else
-    Serial.println("init success");
-  
-  driver.setModeRx();
-  Serial.print("Speed: ");Serial.println(driver.speed());
-  Serial.print("Mode: ");Serial.println(driver.mode());
+    Serial.begin(9600);  // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
 }
 
 void loop()
 {
-  uint8_t buf[12];
-  uint8_t buflen = sizeof(buf);
-  if (driver.recv(buf, &buflen)) // Non-blocking
-    Serial.println((char*)buf);
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+      int i;
+      // Message with a good checksum received, dump it.
+      Serial.print("Message: ");
+      Serial.println((char*)buf);         
+    }
 }
--- sketch_transmitter/sketch_transmitter.ino
+++ sketch_transmitter/sketch_transmitter.ino
@@ -1,26 +1,19 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
-RH_ASK driver(1000);  // txPin 12
+RH_ASK driver;
 
 void setup()
 {
-  Serial.begin(9600);	  // Debugging only
-  delay(2000);
-  if (!driver.init())
-    Serial.println("init failed");
-   else
-    Serial.println("init success");
-  
-  // driver.setModeTx();
-  // Serial.print("Speed: ");Serial.println(driver.speed());
-  // Serial.print("Mode: ");Serial.println(driver.mode());
+    Serial.begin(9600);    // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
 }
 
 void loop()
 {
-  const char *msg = "Hello World!";
-  driver.send((uint8_t *)msg, strlen(msg));
-  driver.waitPacketSent();
-  delay(1000);
+    const char *msg = "Hello World!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
 }
